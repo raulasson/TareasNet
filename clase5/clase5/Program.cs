db6f7c2508424d9cb6bac78c99201cbf1ceb7980
raulasson <raul.indu.13@gmail.com>
2022-05-13 19:21:05 -0300

    Add project files.

---
+++ clase5/clase5/Program.cs
@@ -0,0 +1,18 @@
+﻿var respuesta = "S";
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese un nombre");
+    var nombre = Console.ReadLine();
+    Console.WriteLine($"Hola {nombre}");
+    Console.WriteLine("Desea continuar? (S/N)");
+    respuesta = Console.ReadLine();
+    if (respuesta.ToUpper() == "N")
+    {
+        Console.WriteLine("Programa finalizado correctamente");
+    }
+    else if (respuesta.ToUpper() != "N" && respuesta.ToUpper() != "S")
+    {
+        Console.WriteLine("Opcion no valida");
+    }
+}
---
+++ clase6/clase6/Program.cs
@@ -0,0 +1,23 @@
+﻿int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+int intento = 0;
+int cont = 1;
+while (intento != numeroSecreto)
+{
+    Console.WriteLine("Adivine que numero 1 al 20 genero la computadora");
+    intento = int.Parse(Console.ReadLine());
+    if (intento < numeroSecreto)
+    {
+        Console.WriteLine("El numero ingresado es menor, intente de nuevo");
+    }
+    else if (intento > numeroSecreto)
+    {
+        Console.WriteLine("El numero ingresado es mayor, intente de nuevo");
+    }
+    else
+    {
+        Console.WriteLine($"Felicitaciones, has adivinado el numero secreto que era: {numeroSecreto}");
+        Console.WriteLine($"Lo has logrado en {cont} intentos");
+    }
+    cont++;
+}
