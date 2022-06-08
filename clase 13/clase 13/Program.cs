6603d62267f1a5c0f5fad65ed4d903e38dc781f6
raulasson <raul.indu.13@gmail.com>
2022-06-08 17:11:10 -0300

    ejercicio clase 13

---
+++ clase 13/clase 13/Program.cs
@@ -0,0 +1,42 @@
+﻿using clase_13.modelo;
+decimal total;
+billetera billetera1 = new billetera();
+billetera billetera2 = new billetera();
+
+billetera1.BilletesDe10 = 3;
+billetera1.BilletesDe20 = 1;
+billetera1.BilletesDe50 = 1;
+billetera1.BilletesDe100 = 3;
+billetera1.BilletesDe200 = 1;
+billetera1.BilletesDe500 = 1;
+billetera1.BilletesDe1000 = 3;
+
+billetera2.BilletesDe10 = 5;
+billetera2.BilletesDe20 = 7;
+billetera2.BilletesDe50 = 1;
+billetera2.BilletesDe100 = 3;
+billetera2.BilletesDe200 = 5;
+billetera2.BilletesDe500 = 2;
+billetera2.BilletesDe1000 = 3;
+
+total = billetera1.Total();
+Console.WriteLine($"El total de la billetera1 es : ${total}\n\n");
+
+total = billetera2.Total();
+Console.WriteLine($"El total de la billetera2 es : ${total}\n\n");
+
+Console.WriteLine("Ahora combinamos las billeteras!! \n\n");
+
+billetera billeteracomb = billetera1.Combinar(billetera2);
+Console.WriteLine($"Billetera1: {billetera1.Total()}");
+Console.WriteLine($"Billetera2: {billetera2.Total()}");
+Console.WriteLine($"Billetera combinada: {billeteracomb.Total()}");
+
+Console.WriteLine($"\n\nLos billetes de la billetera combinada son:\n");
+billeteracomb.MostrarBilletes();
+
+
+
+
+
+
---
+++ clase 13/clase 13/modelo/billetera.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_13.modelo
+{
+    internal class billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+
+
+
+
+        public decimal Total()
+        {
+            decimal total;
+            return total = (BilletesDe10 * 10) + (BilletesDe20 * 20) + (BilletesDe50 * 50) +
+            (BilletesDe100 * 100) + (BilletesDe200 * 200) + (BilletesDe500 * 500) + (BilletesDe1000 * 1000);
+        }
+
+        public void MostrarBilletes()
+        {
+            Console.WriteLine($"Billetes de 10: {BilletesDe10}\nBilletes de 20: {BilletesDe20}\nBilletes de 50: {BilletesDe50}\nBilletes de 100: {BilletesDe100}\nBilletes de 200: {BilletesDe200}\nBilletes de 500: {BilletesDe500}\nBilletes de 1000: {BilletesDe1000}");
+        }
+
+        public void Vaciar()
+        {
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe200 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+        }
+        public billetera Combinar(billetera combinar)
+        {
+            billetera combinada = new billetera();
+
+            combinada.BilletesDe10 = BilletesDe10 + combinar.BilletesDe10;
+            combinada.BilletesDe20 = BilletesDe20 + combinar.BilletesDe20;
+            combinada.BilletesDe50 = BilletesDe50 + combinar.BilletesDe50;
+            combinada.BilletesDe100 = BilletesDe100 + combinar.BilletesDe100;
+            combinada.BilletesDe200 = BilletesDe200 + combinar.BilletesDe200;
+            combinada.BilletesDe500 = BilletesDe500 + combinar.BilletesDe500;
+            combinada.BilletesDe1000 = BilletesDe1000 + combinar.BilletesDe1000;
+            this.Vaciar();
+            combinar.Vaciar();
+            return combinada;
+        }
+    }
+}
