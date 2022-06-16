66d0d14a320d2c9505c3072a4fd602fc381037c3
raulasson <raul.indu.13@gmail.com>
2022-06-16 17:14:33 -0300

    clase 14

--- clase 14/clase 14/Program.cs
+++ clase 14/clase 14/Program.cs
@@ -1,1 +1,83 @@
-﻿int[] asd = new int[3];
+﻿using clase_14.modelo;
+
+Baraja baraja = new Baraja();
+
+for (int i = 0; i <baraja.baraja.Length; i++)
+{
+    Carta carta = new Carta();
+    Random randpalo = new Random();
+    Random randnumero = new Random();
+    int numcarta = randnumero.Next(1,13);
+    while (numcarta==8 || numcarta==9)
+    {
+        numcarta = randnumero.Next(1, 13);
+    }
+    int palocarta =randpalo.Next(4);
+    switch (palocarta)
+    {
+        case 0:
+            carta.palo = "Espada";
+            carta.numero = numcarta;
+            baraja.baraja[i] = carta;
+            break;
+        case 1:
+            carta.palo = "Basto";
+            carta.numero = numcarta;
+            baraja.baraja[i] = carta;
+            break;
+        case 2:
+            carta.palo = "Oro";
+            carta.numero = numcarta;
+            baraja.baraja[i] = carta;
+            break;
+        case 3:
+            carta.palo = "Copa";
+            carta.numero = numcarta;
+            baraja.baraja[i] = carta;
+            break;
+    }
+
+}
+
+
+
+for (int i = -1; i <baraja.baraja.Length; i++)
+{
+    Console.WriteLine("Ingrese la opcion a realizar");
+    Console.WriteLine("1_Barajar\n2_Mostrar siguiente carta\n3_Mostrar cartas dispoonibles\n4_Dar cartas\n5_Mostrar cartas del monton\n6_Mostrar baraja\n7_Salir");
+    int respuesta = int.Parse(Console.ReadLine());
+    switch (respuesta)
+    {
+        case 1:
+            baraja.Barajar(i);
+            i--;
+            break;
+        case 2:
+            baraja.SiguienteCarta(i);
+            i--;
+            break;
+        case 3:
+            baraja.CartasDisponibles(i);
+            i--;
+            break;
+        case 4:
+            Console.WriteLine("\n\nCuantas cartas desea?");
+            int cantidad = int.Parse(Console.ReadLine());
+            i= baraja.DarCartas(cantidad, i);
+            break;
+        case 5:
+            baraja.CartasMonton(i);
+            i--;
+            break;
+        case 6:
+            baraja.MostrarBaraja(i);
+            i--;
+            break;
+        default:
+            Environment.Exit(1);
+            break;
+    }
+    
+}
+
+    
--- clase 14/clase 14/modelo/Baraja.cs
+++ clase 14/clase 14/modelo/Baraja.cs
@@ -8,6 +8,92 @@ namespace clase_14.modelo
 {
     internal class Baraja
     {
-        public Carta[] baraja = new Carta[40];
+        static int y = 40;
+        public Carta[] baraja = new Carta[y];
+
+
+        public void Barajar(int i)
+        {
+            Carta[] mezclador = new Carta[y];
+            for (int t = i+1; t < y; t++)
+            {
+                Random j = new Random();
+                int x = j.Next((i+1),y);
+                while (mezclador[x]!=null)
+                {
+                    x = j.Next((i+1),y);
+                }
+                
+                mezclador[x]= baraja[t];
+            }
+            for (int t = i+1; t < y; t++)
+            {
+                baraja[t] = mezclador[t];
+            }
+        }
+
+        public void SiguienteCarta(int i)
+        {
+            if (i == y-1)
+            {
+                Console.WriteLine("No hay mas cartas");
+            }
+            else
+            {
+                Console.WriteLine($"La siguiente carta es:  {baraja[i + 1].numero} de {baraja[i+1].palo}\n\n");
+            }
+        }
+
+        public void CartasDisponibles(int i)
+        {
+            int restantes = (y-1) - i;
+            Console.WriteLine($"Quedan {restantes} cartas restantes");
+        }
+
+        public int DarCartas(int cantidad , int i)
+        {
+            if (cantidad + i>=y)
+            {
+                Console.WriteLine("Hay menos cartas que la cantidad ingresada");
+                return i= i-1;
+            }
+            Console.WriteLine($"Se entregaron {cantidad} cartas al usuario");
+            return i += cantidad - 1;
+        }
+
+        public void CartasMonton(int i)
+        {
+            if (i == -1)
+            {
+                Console.WriteLine("Todavia no ha salido ninguna carta");
+            }
+            else
+            {
+                Console.WriteLine("Las Cartas del monton son:\n");
+                for (int j = i; j >= 0; j--)
+                {
+
+                    Console.WriteLine($"{baraja[j].numero} de {baraja[j].palo}");
+                }
+            }
+        }
+
+        public void MostrarBaraja(int i)
+        {
+            if (i == y - 1)
+            {
+                Console.WriteLine("No hay mas cartas en la baraja");
+            }
+            else
+            {
+                Console.WriteLine("Las cartas de la baraja son:\n");
+                for (int j = i + 1; j < y; j++)
+                {
+                    Console.WriteLine($"{baraja[j].numero} de {baraja[j].palo}");
+                }
+            }       
+        }
+
+
     }
 }
